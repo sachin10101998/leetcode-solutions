e78f74a455e4d3dc6ffe360938f5e45895ef1641
Sachin Chopra <schopra@mt.iitr.ac.in>
2021-09-19 17:06:30 +0530

    Time: 516 ms (45.45%), Space: 182.4 MB (27.27%) - LeetHub

--- maximum-earnings-from-taxi/maximum-earnings-from-taxi.cpp
+++ maximum-earnings-from-taxi/maximum-earnings-from-taxi.cpp
@@ -1,14 +1,42 @@
+using ll= long long;
 class Solution {
 public:
-    long long maxTaxiEarnings(int n, vector<vector<int>>& A) {
-        sort(A.begin(), A.end());
-        vector<long long> dp(n+1);
-        int j = 0;
-        for(int i = 1; i <= n; ++i) {
-            dp[i] = max(dp[i], dp[i - 1]);
-            while (j < A.size() && A[j][0] == i)
-                dp[A[j++][1]] = max(dp[A[j][1]], dp[i] + A[j][1] - A[j][0] + A[j][2]);
+    vector<vector<pair<int,int>>> edges;
+    ll rec(int level,vector<ll>&dp)
+    {
+        if(level==0)
+            return 0;
+        
+        if(dp[level]!=-1){
+            return dp[level];
         }
-        return dp[n];
+        
+        ll ans = rec(level-1,dp); //not picking the passenger at this level.
+        
+        //if picking the passenger check all the best possiblity.
+        for(auto& [src,cost] : edges[level])
+        {
+            ans = max(ans,rec(src,dp) + cost);
+        }
+        
+        return dp[level]=ans;
+        
+    }
+    
+    ll maxTaxiEarnings(int n, vector<vector<int>>& rides) {
+        int R = rides.size();
+        edges.clear();
+        edges.resize(n+5);
+        vector<ll>dp(n+5,-1);
+        
+        for(int i=0;i<R;i++)
+        {
+            int start = rides[i][0], end=rides[i][1];
+            int tip=rides[i][2];
+            int cost=end-start+tip;
+            edges[end].push_back({start,cost});
+        }
+        
+        return rec(n,dp);
     }
 };
