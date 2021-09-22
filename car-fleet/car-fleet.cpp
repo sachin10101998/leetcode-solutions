7844811a3f9cbdbd3a95a0e8ec6e8be7f41a9f6a
Sachin Chopra <schopra@mt.iitr.ac.in>
2021-09-22 21:31:30 +0530

    Time: 176 ms (78.38%), Space: 78.6 MB (60.41%) - LeetHub

---
+++ car-fleet/car-fleet.cpp
@@ -0,0 +1,35 @@
+class Car{
+public:
+    Car(int pos, int speed){
+        this->pos = pos;
+        this->speed= speed;
+    }
+    int pos;
+    int speed;
+};
+
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        vector<Car> cars;
+        int N = position.size();
+        for(int i = 0; i<N; i++){
+            cars.emplace_back(position.at(i), speed.at(i));
+        }
+        
+        sort(cars.begin(), cars.end(), [](const Car& a, const Car& b){
+            return a.pos<b.pos;
+        });
+        
+        stack<float> mono;
+        for(int i = 0; i<N; i++){
+            float time = 
+                (target-cars.at(i).pos)/(float)cars.at(i).speed;
+            while(!mono.empty() && time >= mono.top()){
+                mono.pop();
+            }
+            mono.push(time);
+        }
+        return mono.size();
+    }
+};
