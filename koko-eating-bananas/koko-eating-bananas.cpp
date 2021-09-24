c2d10b12b04bb608f8a4815064a0732a31e0ce94
Sachin Chopra <schopra@mt.iitr.ac.in>
2021-09-25 02:53:50 +0530

    Time: 40 ms (84.87%), Space: 18.8 MB (81.06%) - LeetHub

---
+++ koko-eating-bananas/koko-eating-bananas.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int H) {
+        int lo = 1, hi = pow(10, 9);
+        while (lo < hi) {
+            int mi = lo + (hi - lo) / 2;
+            if (!possible(piles, H, mi))
+                lo = mi + 1;
+            else
+                hi = mi;
+        }
+
+        return lo;
+    }
+
+    // Can Koko eat all bananas in H hours with eating speed K?
+    bool possible(vector<int>& piles, int H, int K) {
+        int time = 0;
+        for (int p: piles)
+            time += (p - 1) / K + 1;
+        return time <= H;
+    }
+};
