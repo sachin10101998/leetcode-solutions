e65229ea2cb89e84fe10d02a865ac3efbd68a0ec
Sachin Chopra <schopra@mt.iitr.ac.in>
2021-10-05 20:22:51 +0530

    Time: 19 ms (5.12%), Space: 8.9 MB (5.03%) - LeetHub

--- bulls-and-cows/bulls-and-cows.cpp
+++ bulls-and-cows/bulls-and-cows.cpp
@@ -1,37 +1,28 @@
 class Solution {
 public:
     string getHint(string secret, string guess) {
-        set<int>s1;
+        unordered_multiset<char>secret_chars;
+        for(auto x: secret){
+            secret_chars.insert(x);
+        }
         int n = secret.length();
-        int bulls =0;
-        int cows = 0;
+        unordered_set<int>correct;
+        int bull = 0;
+        int cows =0;
         for(int i=0;i<n;i++){
             if(secret[i]==guess[i]){
-                bulls++;
-                s1.insert(i);
-            }
-            
-        }
-        map<char,int>m1;
-        for(int i=0;i<n;i++){
-            if(s1.find(i)==s1.end()){
-                m1[secret[i]]++;
+                bull++;
+                correct.insert(i);
+                secret_chars.erase(secret_chars.find(secret[i]));
             }
         }
         for(int i=0;i<n;i++){
-            if(s1.find(i)==s1.end()){
-                if(m1.find(guess[i])==m1.end()){
-                    continue;
-                } else if(m1.find(guess[i])!=m1.end()){
-                    if(m1[guess[i]]>0){
-                        cows++;
-                        m1[guess[i]]--;
-                    }
-                }
+            if(secret[i]!=guess[i]&&secret_chars.find(guess[i])!=secret_chars.end()){
+                secret_chars.erase(secret_chars.find(guess[i]));
+                cows++;
             }
         }
-        string a = to_string(bulls);
-        string b = to_string(cows);
-        return a+"A"+b+"B";
+        return to_string(bull)+"A"+to_string(cows)+"B";
+        
     }
 };
